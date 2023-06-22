ba8f44088ec33f7b9da14793caf67b5e884511bd
Cristhian Recalde <cdrecalde@pucesi.edu.ec>
2023-06-22 17:16:56 -0500

    grados finalizados matr y grad

--- app/Views/DatosEstadisticos/Grados/vista_fe_grad_fecha.php
+++ app/Views/DatosEstadisticos/Grados/vista_fe_grad_fecha.php
@@ -1,3 +1,17 @@
-<?php
-    echo "fecha";
-?>
+<!-- Buton busqueda -->
+
+<div class="container-center m-5 p-3 bg-light rounded shadow-lg p-3 mb-5 bg-body rounded ">
+    <form action="" method="" class="form d-flex" style="justify-content: center;">
+        <div class="input-group-text m-2">
+            <label for="">Desde: </label>
+            <input type="date" class="form-control" placeholder="FechaInicio">
+        </div>
+        <div class="input-group-text m-2">
+            <label for="">Hasta: </label>
+            <input type="date" class="form-control" placeholder="FechaFin">
+        </div>
+        <button type="submit" class="btn btn-primary col-2 m-2">
+            <i class="fa-solid fa-magnifying-glass"> Consultar →</i>
+        </button>
+    </form>
+</div>
