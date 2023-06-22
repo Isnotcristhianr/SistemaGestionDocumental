d228821980673bb3a16bf8d99da35ef86b251664
Cristhian Recalde <cdrecalde@pucesi.edu.ec>
2023-06-22 17:51:08 -0500

    finalizado filtrado

--- app/Views/DatosEstadisticos/Tecnologias/vista_fe_tec_carreras.php
+++ app/Views/DatosEstadisticos/Tecnologias/vista_fe_tec_carreras.php
@@ -1,3 +1,16 @@
-<?php   
-    echo "Estadisticas de Carreras";
-?>
+<!-- Buton busqueda -->
+<div class="container-center m-5 p-3 bg-light rounded col-xs-6 shadow-lg p-3 mb-5 bg-body rounded">
+    <div class="row ">
+        <div class="col-12">
+            <!-- btn -->
+            <div class="d-grid gap-2 col-2 mx-auto m-2">
+                <a href="<?php echo base_url('') ?>" class="btn btn-dark " type="button">Matriculados</a>
+            </div>
+
+            <div class="d-grid gap-2 col-2 mx-auto m-2">
+                <a href="<?php echo base_url('') ?>" class="btn btn-dark" type="button">Graduados</a>
+            </div>
+
+        </div>
+    </div>
+</div>
--- app/Views/DatosEstadisticos/Tecnologias/vista_fe_tec_fecha.php
+++ app/Views/DatosEstadisticos/Tecnologias/vista_fe_tec_fecha.php
@@ -1,3 +1,17 @@
-<?php
-echo "Fecha";
-?>
+<!-- Buton busqueda -->
+
+<div class="container-center m-5 p-3 bg-light rounded shadow-lg p-3 mb-5 bg-body rounded ">
+    <form action="" method="" class="form d-flex" style="justify-content: center;">
+        <div class="input-group-text m-2">
+            <label for="">Desde: </label>
+            <input type="date" class="form-control" placeholder="FechaInicio">
+        </div>
+        <div class="input-group-text m-2">
+            <label for="">Hasta: </label>
+            <input type="date" class="form-control" placeholder="FechaFin">
+        </div>
+        <button type="submit" class="btn btn-primary col-2 m-2">
+            <small class="fs-6 fw-semibold">Consultar </small><i class="fa-solid fa-magnifying-glass"> </i>
+        </button>
+    </form>
+</div>
