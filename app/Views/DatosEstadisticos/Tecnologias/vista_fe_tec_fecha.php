7e6d22b694edd0f44e332fc184d4061afff4687c
Cristhian Recalde <cdrecalde@pucesi.edu.ec>
2023-06-23 15:05:57 -0500

    se

--- app/Views/DatosEstadisticos/Tecnologias/vista_fe_tec_fecha.php
+++ app/Views/DatosEstadisticos/Tecnologias/vista_fe_tec_fecha.php
@@ -1,5 +1,4 @@
 <!-- Buton busqueda -->
-
 <div class="container-center m-5 p-3 bg-light rounded shadow-lg p-3 mb-5 bg-body rounded ">
     <form action="" method="" class="form d-flex" style="justify-content: center;">
         <div class="input-group-text m-2">
