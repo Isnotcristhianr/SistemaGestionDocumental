07792861642d080fc3061dd802148856d5143319
Cristhian Recalde <cdrecalde@pucesi.edu.ec>
2023-06-30 15:45:02 -0500

    implemetacion oferta academica vigente e historica en periodos

--- app/Views/DatosEstadisticos/Grados/busqueda/vista_b_periodo_matr.php
+++ app/Views/DatosEstadisticos/Grados/busqueda/vista_b_periodo_matr.php
@@ -16,8 +16,12 @@
     </div>
 
     <!-- Contenido-->
+    <h3 class="text text-start text-success">Oferta Académica Vigente</h3>
+    <a href="" class="btn btn-success ">Ver Resumen</a>
+    <br>
+    <br>
     <div class="table-responsive text-center">
-        <table class="table align-middle order-column hover nowrap row-border stripe " id="tbl">
+        <table class="table table-success align-middle order-column hover nowrap row-border stripe " id="">
             <thead>
                 <th hidden>ID</th>
                 <th>Año</th>
@@ -25,23 +29,69 @@
                 <th>Acciones</th>
             </thead>
             <tbody>
-                <!-- llenar toda la tabla -->
+                <!-- llenar toda la tabla con fecha de año actual-->
                 <?php
-
                 foreach ($tbl_periodo as $periodo) {
+                    if ($periodo['PER_ANO'] == date('Y')) {
+
                 ?>
-                    <tr>
-                        <td hidden><?php echo $periodo['PER_ID']; ?></td>
-                        <td><?php echo $periodo['PER_ANO']; ?></td>
-                        <td><?php echo $periodo['PER_PERIODO']; ?></td>
-                        <td><a href="" class="btn btn-success">Visualizar →</a></td>
-                    </tr>
+                        <tr>
+                            <td hidden><?php echo $periodo['PER_ID']; ?></td>
+                            <td><?php echo $periodo['PER_ANO']; ?></td>
+                            <td><?php echo $periodo['PER_PERIODO']; ?></td>
+                            <td><a href="" class="btn btn-success">Visualizar →</a></td>
+                        </tr>
                 <?php
+                    }
                 }
                 ?>
-
 
             </tbody>
         </table>
     </div>
+</div>
+
+
+<div class="container-center m-5 p-3 bg-light rounded col-xs-6 shadow-lg p-3 mb-5 bg-body rounded">
+    <h3 class="text text-start text-primary">Oferta Académica Histórico</h3>
+    <a href="" class="btn btn-primary">Ver Resumen</a>
+    <br>
+    <br>
+    <div class="table-responsive text-center">
+
+        <!-- Llenar tabla con no ativas -->
+        <table class="table align-middle order-column hover nowrap row-border stripe " id="tbl2">
+            <thead>
+                <th hidden>ID</th>
+                <th>Año</th>
+                <th>Periodo</th>
+                <th>Acciones</th>
+            </thead>
+            <tbody>
+                <!-- llenar toda la tabla sin contar el año actual-->
+                <?php
+                foreach ($tbl_periodo as $periodo) {
+                    if ($periodo['PER_ANO'] != date('Y')) {
+
+                ?>
+                        <tr>
+                            <td hidden><?php echo $periodo['PER_ID']; ?></td>
+                            <td><?php echo $periodo['PER_ANO']; ?></td>
+                            <td><?php echo $periodo['PER_PERIODO']; ?></td>
+                            <td><a href="" class="btn btn-primary">Visualizar →</a></td>
+                        </tr>
+                <?php
+
+                    }
+                }
+                ?>
+
+            </tbody>
+        </table>
+
+
+
+
+    </div>
+
 </div>
