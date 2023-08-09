33841dd2cb12bd108270d434793df5d59644a93b
Cristhian David Recalde <cdrecalde@pucesi.edu.ec>
2023-08-09 16:56:18 -0500

    corrigiendo tabla de reporte general

--- app/Views/graficasEstadisticas/historico/vistaEstHistorico.php
+++ app/Views/graficasEstadisticas/historico/vistaEstHistorico.php
@@ -32,22 +32,47 @@
         <a href="<?php echo base_url('index.php/ControladorReportes/reporteTitulacion') ?>" class="btn btn-info btn-sm">
             <i class="fas fa-print"></i> Imprimir
         </a>
+        <!-- ENVIAR IMG A CORREO -->
+        <a href="<?php echo base_url('index.php/ControladorReportes/reporteTitulacionImg') ?>" class="btn btn-secondary btn-sm">
+            <i class="fas fa-envelope"></i> Enviar a Correo
+        </a>
     </div>
     <!-- Reporte Estadistico -->
     <canvas id="myChart"></canvas>
     <!-- Script chart -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <!-- grafica -->
-    
+
     <script>
         //obtener datos de php del controlador
         var datos = <?php echo json_encode($tbl_estadistica_matriz) ?>;
+        var peri = <?php echo json_encode($tbl_periodo) ?>;
 
         //preparar los datos para el grafico
         var total = datos.map(dato => dato.ESTM_TOTAL); //eje x
-        //eje y
+        //eje y /* logica del periodo */ var periodo = datos.map(dato => dato.ESTM_PERIODO);          
         //logica del periodo
-        var periodo = datos.map(dato => dato.ESTM_PERIODO);
+        //PER_ID, PER_ANO, ESTM_PERIODO, se comparan los datos de la tabla periodo con la tabla matriz
+        //si el PER_ID de la tabla periodo es igual al ESTM_PERIODO de la tabla matriz se le asigna el PER_ANO
+        periodo = datos.map(dato => {
+            for (let i = 0; i < peri.length; i++) {
+                if (peri[i].PER_ID == dato.ESTM_PERIODO) {
+                    return peri[i].PER_ANO;
+                }
+            }
+        });
+        //ordenar los anios en eje y ascendente
+        periodo.sort(function(a, b) {
+            return a - b;
+        });
+        //eliminar duplicados y sumar los totales de los duplicados
+        var total = total.reduce((a, b) => {
+            return a + b;
+        }, 0);
+        var periodo = periodo.filter((value, index) => {
+            return periodo.indexOf(value) === index;
+        });
+        
 
         //grafico de barras
         var ctx = document.getElementById('myChart').getContext('2d');
@@ -80,10 +105,6 @@
                     text: 'Total de Graduados por Periodo'
                 },
                 //animacion
-                animation: {
-                    duration: 2000,
-                    easing: 'easeInOutBounce'
-                },
                 //leyenda
                 legend: {
                     display: true,
@@ -92,22 +113,16 @@
                         fontColor: '#000'
                     }
                 },
-                //diseño responsivo
-                responsive: true,
-                maintainAspectRatio: false,
+                //eje y
                 scales: {
                     yAxes: [{
                         ticks: {
-                            beginAtZero: true,
-                            fontColor: '#000'
-                        }
-                    }],
-                    xAxes: [{
-                        ticks: {
-                            fontColor: '#000'
+                            beginAtZero: true
                         }
                     }]
-                }
+                },
+                //responsive
+                responsive: true,
             }
         });
     </script>
