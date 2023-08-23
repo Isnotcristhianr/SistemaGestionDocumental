8635d70109fad59ad850d9b4b3efe34a02ab4f56
Cristhian David Recalde <cdrecalde@pucesi.edu.ec>
2023-08-23 12:55:51 -0500

    opciones de descargas mejoradas e implementadas, falta mejorar el export a excel esta muy simple

--- app/Views/graficasEstadisticas/historico/vistaEstHistorico.php
+++ app/Views/graficasEstadisticas/historico/vistaEstHistorico.php
@@ -365,16 +365,259 @@
     <!-- Descargas -->
     <script>
         //exportar a excel
-        document.getElementById('exportExcel').addEventListener('click', function() {
-            //obtener los datos del canvas y convertirlos a excel
-            var url_base64jp = document.getElementById("myChart").toDataURL("image/jpg");
-            var a = document.getElementById("exportExcel");
-            a.href = url_base64jp;
-            
-            //nombre del archivo
-            a.download = "ReporteEstadisticoHistorico.xls";
-                 
+        document.getElementById('exportExcel').addEventListener('click', async function() {
+
+            mostrarAlertaExcel();
+            //esperar 3 seg
+            await new Promise(r => setTimeout(r, 3000));
+
+            //! Por Años General
+            {
+                //* eje y -> total de estudiantes min 0 y max suma de ESTM_TOTAL
+                //? logica-> total
+                var total = [];
+                //recorrer datos
+                for (let i = 0; i < datos.length; i++) {
+                    //agregar total
+                    total.push(datos[i].ESTM_TOTAL);
+                }
+
+                //* eje x -> periodo
+                //? logica-> periodo
+                var periodo = [];
+                //recorrer datos
+                for (let i = 0; i < datos.length; i++) {
+                    //recorrer peri
+                    for (let j = 0; j < peri.length; j++) {
+                        //comparar PER_ID con ESTM_PERIODO
+                        if (datos[i].ESTM_PERIODO == peri[j].PER_ID) {
+                            //agregar PER_ANO a periodo
+                            periodo.push(peri[j].PER_ANO);
+                        }
+                    }
+                }
+                //sumar los totales de cada año y mostrar por consola el total de cada año, transformar string a int para sumar
+                //recorrer periodo
+                for (let i = 0; i < periodo.length; i++) {
+                    //acumulador
+                    var suma = 0;
+                    //recorrer datos
+                    for (let j = 0; j < datos.length; j++) {
+                        //recorrer peri
+                        for (let k = 0; k < peri.length; k++) {
+                            //comparar PER_ID con ESTM_PERIODO
+                            if (datos[j].ESTM_PERIODO == peri[k].PER_ID) {
+                                //comparar PER_ANO con periodo
+                                if (peri[k].PER_ANO == periodo[i]) {
+                                    //acumular total
+                                    suma += parseInt(datos[j].ESTM_TOTAL);
+                                }
+                            }
+                        }
+                    }
+                    //eliminar años repetidos de periodo y su total asignado
+                    periodo = periodo.filter((value, index) => periodo.indexOf(value) === index);
+
+                    //mostrar total por cada año
+                    //  console.log(" Total de Estudiantes " + periodo[i] + ": " + suma);
+
+                    //enviar a la grafica
+                    total[i] = suma;
+
+                    //ordenar periodo de menor a mayor
+                    periodo.sort(function(a, b) {
+                        return a - b;
+                    });
+                }
+            }
+
+            //!Por Años Genero Masculino -> ESTM_GENERO_H
+            {
+                //NUEVO TOTAL DE ESTUDIANTES H
+                var totalH = [];
+                //recorrer datos
+                for (let i = 0; i < datos.length; i++) {
+                    //agregar total
+                    totalH.push(datos[i].ESTM_GENERO_H);
+                }
+
+                //* eje x -> periodo
+                //? logica-> periodo
+                var periodoH = [];
+                //recorrer datos
+                for (let i = 0; i < datos.length; i++) {
+                    //recorrer peri
+                    for (let j = 0; j < peri.length; j++) {
+                        //comparar PER_ID con ESTM_PERIODO
+                        if (datos[i].ESTM_PERIODO == peri[j].PER_ID) {
+                            //agregar PER_ANO a periodo
+                            periodoH.push(peri[j].PER_ANO);
+                        }
+                    }
+                }
+                //sumar los ESTM_GENERO_M de cada año y mostrar por consola el total de cada año, transformar string a int para sumar
+                //recorrer periodo
+                for (let i = 0; i < periodoH.length; i++) {
+                    //acumulador
+                    var sumaH = 0;
+                    //recorrer datos
+                    for (let j = 0; j < datos.length; j++) {
+                        //recorrer peri
+                        for (let k = 0; k < peri.length; k++) {
+                            //comparar PER_ID con ESTM_PERIODO
+                            if (datos[j].ESTM_PERIODO == peri[k].PER_ID) {
+                                //comparar PER_ANO con periodo
+                                if (peri[k].PER_ANO == periodoH[i]) {
+                                    //acumular total
+                                    sumaH += parseInt(datos[j].ESTM_GENERO_H);
+                                }
+                            }
+                        }
+                    }
+                    //eliminar años repetidos de periodo y su total asignado
+                    periodoH = periodoH.filter((value, index) => periodoH.indexOf(value) === index);
+
+                    //mostrar total por cada año
+                    //  console.log(" Total de Estudiantes Masculino " + periodoH[i] + ": " + sumaH);
+
+                    //enviar a la grafica
+                    totalH[i] = sumaH;
+
+                    //ordenar periodo de menor a mayor
+                    periodoH.sort(function(a, b) {
+                        return a - b;
+                    });
+                }
+            }
+
+            //! Por Años Genero Femenino -> ESTM_GENERO_M
+            {
+                //Nuevo total de estudiantes M
+                var totalM = [];
+                //recorrer datos
+                for (let i = 0; i < datos.length; i++) {
+                    //agregar total
+                    totalM.push(datos[i].ESTM_GENERO_M);
+                }
+
+                //* eje x -> periodo
+                //? logica-> periodo
+                var periodoM = [];
+                //recorrer datos
+                for (let i = 0; i < datos.length; i++) {
+                    //recorrer peri
+                    for (let j = 0; j < peri.length; j++) {
+                        //comparar PER_ID con ESTM_PERIODO
+                        if (datos[i].ESTM_PERIODO == peri[j].PER_ID) {
+                            //agregar PER_ANO a periodo
+                            periodoM.push(peri[j].PER_ANO);
+                        }
+                    }
+                }
+                //sumar los ESTM_GENERO_M de cada año y mostrar por consola el total de cada año, transformar string a int para sumar
+                //recorrer periodo
+                for (let i = 0; i < periodoM.length; i++) {
+                    //acumulador
+                    var sumaM = 0;
+                    //recorrer datos
+                    for (let j = 0; j < datos.length; j++) {
+                        //recorrer peri
+                        for (let k = 0; k < peri.length; k++) {
+                            //comparar PER_ID con ESTM_PERIODO
+                            if (datos[j].ESTM_PERIODO == peri[k].PER_ID) {
+                                //comparar PER_ANO con periodo
+                                if (peri[k].PER_ANO == periodoM[i]) {
+                                    //acumular total
+                                    sumaM += parseInt(datos[j].ESTM_GENERO_M);
+                                }
+                            }
+                        }
+                    }
+                    //eliminar años repetidos de periodo y su total asignado
+                    periodoM = periodoM.filter((value, index) => periodoM.indexOf(value) === index);
+
+                    //mostrar total por cada año
+                    // console.log(" Total de Estudiantes Femenino " + periodoM[i] + ": " + sumaM);
+
+                    //enviar a la grafica
+                    totalM[i] = sumaM;
+
+                    //ordenar periodo de menor a mayor
+                    periodoM.sort(function(a, b) {
+                        return a - b;
+                    });
+                }
+
+
+            }
+
+            //obtener los datos y convertirlos para excel
+            var data = [];
+            //recorrer periodo
+            for (let i = 0; i < periodo.length; i++) {
+                //agregar datos
+                data.push({
+                    Año: periodo[i],
+                    Total: total[i],
+                    Hombres: totalH[i],
+                    Mujeres: totalM[i]
+                });
+            }
+
+            //exportar a excel
+            exportToCsv('ReporteEstadisticoHistorico.csv', data);
+
+
+
         });
+        //alerta excel
+        function mostrarAlertaExcel() {
+            Command: toastr["success"]("Se esta exportando el reporte a Excel por favor espere a que se genere el documento", "Excel")
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": true,
+                "progressBar": true,
+                "positionClass": "toast-top-right",
+                "preventDuplicates": true,
+                "onclick": null,
+                "showDuration": "100",
+                "hideDuration": "500",
+                "timeOut": "2500",
+                "extendedTimeOut": "500",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            }
+
+        }
+        //exportar a excel
+        function exportToCsv(filename, data) {
+            var csv = "data:text/csv;charset=utf-8,";
+
+            // Agregar encabezados y ajustar al ancho 
+            var headers = Object.keys(data[0]);
+            csv += headers.join(";") + "\n";
+
+
+
+            // Agregar filas de datos
+            data.forEach(function(row) {
+                var values = headers.map(function(header) {
+                    return row[header];
+                });
+                csv += values.join(";") + "\n";
+            });
+
+            // Crear un enlace de descarga y hacer clic en él
+            var encodedUri = encodeURI(csv);
+            var link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", filename);
+            document.body.appendChild(link);
+            link.click();
+        }
 
         //Exportar a pdf
         document.getElementById('exportPdf').addEventListener('click', async function() {
