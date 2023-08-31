507dc162da5fc398c2bb1b32cd92c9803be44ab4
Cristhian David Recalde <cdrecalde@pucesi.edu.ec>
2023-08-31 12:00:32 -0500

    correcion de redirecionamiento

--- app/Views/DatosEstadisticos/PosGrados/busqueda/vista_b_carrera_matr.php
+++ app/Views/DatosEstadisticos/PosGrados/busqueda/vista_b_carrera_matr.php
@@ -2,7 +2,7 @@
 
 <div class="container-center m-5 p-3 bg-light rounded col-xs-6 shadow-lg p-3 mb-5 bg-body rounded">
     <!-- Volver -->
-    <a href="<?php echo base_url('index.php/FiltroEstadisticoPosgrado') ?>" class="btn btn-outline-primary">← Volver</a>
+    <a href="http://localhost/SistemaGestionDocumental/index.php/FiltroEstadisticoPosgradoBusqueda/Posgrado/Carrera" class="btn btn-outline-primary">← Volver</a>
     <div class="row ">
         <div class="col-12">
             <h2 class="text-center text-primary">Datos Estadísticos Posgrados
