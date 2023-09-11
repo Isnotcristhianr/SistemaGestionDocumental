70bb962fdeefe4cca54783278c57327aefeadc76
Cristhian David Recalde <cdrecalde@pucesi.edu.ec>
2023-09-11 09:26:25 -0500

    cambios correctos

--- app/Controllers/ControladorCalendario.php
+++ app/Controllers/ControladorCalendario.php
@@ -49,7 +49,26 @@ class ControladorCalendario extends BaseController
             echo view('header');
             echo view('calendarioAcademico/ver', ['archivos' => $archivos, 'nombre' => $nombre]);
             echo view('footer');
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    }
 
+    //ver periodo
+    public function verPeriodo($nombre, $archivo)
+    {
+        try {
+
+            if ($nombre == 'POSGRADO') {
+                $directorio = 'C:\XAMPP\htdocs\SistemaGestionDocumental\public\files\CALENDARIOS ACADÉMICOS\POSGRADO';
+            } elseif ($nombre == 'PREGRADO') {
+                $directorio = 'C:\XAMPP\htdocs\SistemaGestionDocumental\public\files\CALENDARIOS ACADÉMICOS\PREGRADO';
+            } elseif ($nombre == 'PUCETEC') {
+                $directorio = 'C:\XAMPP\htdocs\SistemaGestionDocumental\public\files\CALENDARIOS ACADÉMICOS\PUCETEC';
+            }
+
+            // Construye la ruta completa al archivo
+            $rutaArchivo = $directorio . '\\' . $archivo;
         } catch (\Exception $e) {
             die($e->getMessage());
         }
@@ -57,27 +76,26 @@ class ControladorCalendario extends BaseController
 
     //ver archivo
     public function verArchivo($nombre, $archivo)
-{
-    try {
-        // Construye la ruta completa al archivo
-        $rutaArchivo = 'C:\XAMPP\htdocs\SistemaGestionDocumental\public\files\CALENDARIOS ACADÉMICOS\\' . $nombre . '\\' . $archivo;
+    {
+        try {
+            // Construye la ruta completa al archivo
+            $rutaArchivo = 'C:\XAMPP\htdocs\SistemaGestionDocumental\public\files\CALENDARIOS ACADÉMICOS\\' . $nombre . '\\' . $archivo;
 
-        // Comprueba si el archivo existe y es accesible
-        if (file_exists($rutaArchivo)) {
-            // Configura las cabeceras para mostrar o descargar el archivo
-            header('Content-Type: application/pdf'); // Cambia el tipo de archivo según sea necesario
-            header('Content-Disposition: inline; filename="' . $archivo . '"');
-            header('Content-Length: ' . filesize($rutaArchivo));
+            // Comprueba si el archivo existe y es accesible
+            if (file_exists($rutaArchivo)) {
+                // Configura las cabeceras para mostrar o descargar el archivo
+                header('Content-Type: application/pdf'); // Cambia el tipo de archivo según sea necesario
+                header('Content-Disposition: inline; filename="' . $archivo . '"');
+                header('Content-Length: ' . filesize($rutaArchivo));
 
-            // Lee y muestra el contenido del archivo
-            readfile($rutaArchivo);
-        } else {
-            // Si el archivo no existe, puedes mostrar un mensaje de error o redirigir a una página de error.
-            echo 'El archivo no existe o no es accesible.';
+                // Lee y muestra el contenido del archivo
+                readfile($rutaArchivo);
+            } else {
+                // Si el archivo no existe, puedes mostrar un mensaje de error o redirigir a una página de error.
+                echo 'El archivo no existe o no es accesible.';
+            }
+        } catch (\Exception $e) {
+            die($e->getMessage());
         }
-    } catch (\Exception $e) {
-        die($e->getMessage());
     }
-}
-
 }
--- app/Views/calendarioAcademico/ver.php
+++ app/Views/calendarioAcademico/ver.php
@@ -5,19 +5,72 @@
             <h4 class="text-center text-dark"><?= $nombre; ?></h4>
         </div>
     </div>
-    <div class="card m-3 shadow p-2">
-        <h3>Archivos de <?= $nombre; ?></h3>
+    <div class="card m-3 p-3 shadow">
+        <h3>Archivos de: <?= $nombre; ?></h3>
         <div class="card-body">
-            <!-- Lista de archivos -->
-            <ul>
-                <?php foreach ($archivos as $archivo) : ?>
-                    <li>
-                        <a href="<?= base_url('index.php/calendarioAcademico/verArchivo/' . $nombre . '/' . $archivo); ?>">
-                            <?= $archivo; ?>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+            <!-- Lista de archivos, cuadricula horizontal -->
+            <label for="" class="fs-2">Vista</label>
+            <div>
+                <input type="radio" name="grupo" id="list" checked>
+                <label for="list">
+                    <i class="fa-solid fa-list"></i>
+                    Lista</label>
+                <input type="radio" name="grupo" id="grid">
+                <label for="grid">
+                    <i class="fa-solid fa-grip"></i>
+                    Cuadricula</label>
+            </div>
+
+            <div id="vista">
+                <!-- Contenedor para la vista de lista -->
+                <div id="listaArchivos" style="display: block;">
+                    <ul>
+                        <?php foreach ($archivos as $archivo) : ?>
+                            <li>
+                                <a href="<?= base_url('index.php/calendarioAcademico/verArchivo/' . $nombre . '/' . $archivo); ?>" style="text-decoration: none;" class="color">
+                                    <img src="../../../public/imgs/files.png" alt="files" width="80">
+                                    <?= $archivo; ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+
+                <!-- Contenedor para la vista de cuadrícula (inicialmente oculto) -->
+                <div id="gridArchivos" style="display: none;">
+                    <?php foreach ($archivos as $archivo) : ?>
+                        <div style="display: inline-block; margin: 10px;" class="card">
+                            <a href="<?= base_url('index.php/calendarioAcademico/verArchivo/' . $nombre . '/' . $archivo); ?>" style="text-decoration: none;" class="color">
+                                <img src="../../../public/imgs/files.png" alt="files" width="100">
+                                <br>
+                                <?= $archivo; ?>
+                            </a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+
+            <script>
+                // Obtener elementos HTML
+                const list = document.getElementById('list');
+                const grid = document.getElementById('grid');
+                const listaArchivos = document.getElementById('listaArchivos');
+                const gridArchivos = document.getElementById('gridArchivos');
+
+                // Agregar oyentes de eventos para los radio buttons
+                list.addEventListener('change', mostrarVista);
+                grid.addEventListener('change', mostrarVista);
+
+                function mostrarVista() {
+                    if (list.checked) {
+                        listaArchivos.style.display = 'block';
+                        gridArchivos.style.display = 'none';
+                    } else if (grid.checked) {
+                        listaArchivos.style.display = 'none';
+                        gridArchivos.style.display = 'block';
+                    }
+                }
+            </script>
         </div>
     </div>
 </div>
