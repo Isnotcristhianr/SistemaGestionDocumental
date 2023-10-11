ed512f41f488193320c3616b85f10fe2451216f8
Cristhian David Recalde <cdrecalde@pucesi.edu.ec>
2023-10-11 09:05:39 -0500

    selecotr data table implementado en periodos

--- app/Views/DatosEstadisticos/Grados/busqueda/vista_b_periodo_grad.php
+++ app/Views/DatosEstadisticos/Grados/busqueda/vista_b_periodo_grad.php
@@ -12,16 +12,17 @@
             <h4 class="text-center text-dark">Búsqueda: Periodo
             </h4>
         </div>
-        <div class="col-12">
-            <h5 class="text-center text-secondary"> Graduados </h5>
+        <div class="col-12 m-2">
+            <h5 class="text-center text-secondary">↓ Graduados ↓
+                <br>
+                <a id="btnBox"></a>
+            </h5>
         </div>
     </div>
 
     <!-- Contenido-->
     <h3 class="text text-start text-success">Oferta Académica Vigente</h3>
     <br>
-    <div id="seleccionvigente" hidden></div>
-    <a id="btnvigente"></a>
     <table class="table table-success align-middle order-column hover row-border stripe " id="tbl">
         <thead>
             <th hidden>ID</th>
@@ -48,7 +49,7 @@
                         <td hidden><?php echo $periodo['PER_ID']; ?></td>
                         <td>
                             <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" name="seleccionar[]" value="<?php $periodo['PER_ID']; ?>">
+                                <input class="form-check-input" type="checkbox" name="seleccionar[]" value="<?php echo $periodo['PER_ID']; ?>">
                             </div>
                         </td>
                         <td><?php echo $periodo['PER_ANO']; ?></td>
@@ -71,8 +72,6 @@
 <div class="container-center m-5 p-3 bg-light rounded col-xs-6 shadow-lg p-3 mb-5 bg-body rounded">
     <h3 class="text text-start text-primary">Oferta Académica Histórico</h3>
     <br>
-    <div id="seleccionHistorico" hidden></div>
-    <a id="btnHistorico"></a>
     <!-- Llenar tabla con no ativas -->
     <table class="table table-primary align-middle order-column hover row-border stripe " id="tbl2">
         <thead>
@@ -92,7 +91,7 @@
                         <td hidden><?php echo $periodo['PER_ID']; ?></td>
                         <td>
                             <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" name="seleccionar[]" value="<?php $periodo['PER_ID']; ?>">
+                                <input class="form-check-input" type="checkbox" name="seleccionar[]" value="<?php echo $periodo['PER_ID']; ?>">
                             </div>
                         </td>
                         <td><?php echo $periodo['PER_ANO']; ?></td>
@@ -109,3 +108,35 @@
         </tbody>
     </table>
 </div>
+
+<script>
+    // Obtener todos los checkboxes con name="seleccionar[]"
+    const checkboxes = document.querySelectorAll('input[name="seleccionar[]"]');
+
+    // Agregar un evento de cambio a cada checkbox
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', function() {
+            const valoresSeleccionados = Array.from(checkboxes)
+                .filter((cb) => cb.checked)
+                .map((cb) => cb.value);
+        });
+
+        //crear btn si hay al menos una seleccion
+        checkbox.addEventListener('change', function() {
+            if (document.querySelectorAll('input[name="seleccionar[]"]:checked').length > 0) {
+                document.getElementById('btnBox').innerHTML = '<br><a href="#" class="btn btn-primary"><i class="fa-solid fa-circle-info p-1"></i>Generar Reporte Selección<i class="fa-solid fa-circle-info p-1"></i></a><br>';
+            } else {
+                document.getElementById('btnBox').innerHTML = '';
+            }
+        });
+    });
+
+    //al hacer click en btn capturar valores seleccionados 
+    document.getElementById('btnBox').addEventListener('click', function() {
+        const valoresSeleccionados = Array.from(checkboxes)
+            .filter((cb) => cb.checked)
+            .map((cb) => cb.value);
+
+        alert(valoresSeleccionados);
+    });
+</script>
--- app/Views/DatosEstadisticos/Grados/busqueda/vista_b_periodo_matr.php
+++ app/Views/DatosEstadisticos/Grados/busqueda/vista_b_periodo_matr.php
@@ -11,16 +11,17 @@
             <h4 class="text-center text-dark">Búsqueda: Periodo
             </h4>
         </div>
-        <div class="col-12">
-            <h5 class="text-center text-secondary"> Matriculados </h5>
+        <div class="col-12 m-2">
+            <h5 class="text-center text-secondary">↓ Matriculados ↓
+                <br>
+                <a id="btnBox"></a>
+            </h5>
         </div>
     </div>
 
     <!-- Contenido-->
     <h3 class="text text-start text-success">Oferta Académica Vigente</h3>
     <br>
-    <div id="seleccionvigente" hidden></div>
-    <a id="btnvigente"></a>
     <table class="table table-success align-middle order-column hover row-border stripe " id="tbl">
         <thead>
             <th hidden>ID</th>
@@ -47,7 +48,7 @@
                     <td hidden><?php echo $periodo['PER_ID']; ?></td>
                     <td>
                         <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" name="seleccionar[]" value="<?php $periodo['PER_ID']; ?>">
+                            <input class="form-check-input" type="checkbox" name="seleccionar[]" value="<?php echo $periodo['PER_ID']; ?>">
                         </div>
                     </td>
                     <td><?php echo $periodo['PER_ANO']; ?></td>
@@ -70,8 +71,6 @@
     <h3 class="text text-start text-primary">Oferta Académica Histórico</h3>
     <a href="" class="btn btn-primary">Ver Resumen</a>
     <br>
-    <div id="seleccionHistorico" hidden></div>
-    <a id="btnHistorico"></a>
     <!-- Llenar tabla con no ativas -->
     <table class="table table-primary align-middle order-column hover row-border stripe " id="tbl2">
         <thead>
@@ -92,7 +91,7 @@
                         <td hidden><?php echo $periodo['PER_ID']; ?></td>
                         <td>
                             <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" name="seleccionar[]" value="<?php $periodo['PER_ID']; ?>">
+                                <input class="form-check-input" type="checkbox" name="seleccionar[]" value="<?php echo $periodo['PER_ID']; ?>">
                             </div>
                         </td>
                         <td><?php echo $periodo['PER_ANO']; ?></td>
@@ -111,3 +110,35 @@
         </tbody>
     </table>
 </div>
+
+<script>
+    // Obtener todos los checkboxes con name="seleccionar[]"
+    const checkboxes = document.querySelectorAll('input[name="seleccionar[]"]');
+
+    // Agregar un evento de cambio a cada checkbox
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', function() {
+            const valoresSeleccionados = Array.from(checkboxes)
+                .filter((cb) => cb.checked)
+                .map((cb) => cb.value);
+        });
+
+        //crear btn si hay al menos una seleccion
+        checkbox.addEventListener('change', function() {
+            if (document.querySelectorAll('input[name="seleccionar[]"]:checked').length > 0) {
+                document.getElementById('btnBox').innerHTML = '<br><a href="#" class="btn btn-primary"><i class="fa-solid fa-circle-info p-1"></i>Generar Reporte Selección<i class="fa-solid fa-circle-info p-1"></i></a><br>';
+            } else {
+                document.getElementById('btnBox').innerHTML = '';
+            }
+        });
+    });
+
+    //al hacer click en btn capturar valores seleccionados 
+    document.getElementById('btnBox').addEventListener('click', function() {
+        const valoresSeleccionados = Array.from(checkboxes)
+            .filter((cb) => cb.checked)
+            .map((cb) => cb.value);
+
+        alert(valoresSeleccionados);
+    });
+</script>
