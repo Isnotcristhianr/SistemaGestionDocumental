ab730599b5b5b75836f3c6d3e7dcb92ccdab3ef5
Cristhian David Recalde <cdrecalde@pucesi.edu.ec>
2023-11-07 10:05:29 -0500

    correccion pequeño bug redireccionamiento

--- app/Views/graficasEstadisticas/posgrado/vistaPosGenGrad.php
+++ app/Views/graficasEstadisticas/posgrado/vistaPosGenGrad.php
@@ -1,6 +1,6 @@
 <div class="container-center m-5 p-3 bg-light rounded col-xs-6 shadow-lg p-3 mb-5 bg-body rounded">
 
-    <a href="<?php echo base_url('index.php/FiltroEstadisticoPosgradoBusqueda/Posgrado/Graduados') ?>" class="btn btn-outline-primary">
+<a href="<?php echo base_url('index.php/FiltroEstadisticoPosgradoBusqueda/Posgrado/General') ?>" class="btn btn-outline-primary">
         <i class="fa-solid fa-caret-left"></i> Volver
     </a>
     <div class="row ">
